89bde634844e2e68c36683005a7dc0ba6096f6cc
ero-s <austinelomcso@gmail.com>
2024-11-24 01:55:24 +0800

    redid recent implementations plus ui

--- src/object/OBJ_ManaCrystal.java
+++ src/object/OBJ_ManaCrystal.java
@@ -4,14 +4,12 @@ import Entity.Entity;
 import pkg2dgame.GamePanel;
 
 public class OBJ_ManaCrystal extends Entity{
-    GamePanel gp;
 
     public OBJ_ManaCrystal(GamePanel gp){
         super(gp);
-        this.gp = gp;
 
         name = "Mana Crystal";
-        image = setup("/pics/objects/manacrystal_full.png", gp.tileSize, gp.tileSize);
-        image2 = setup("/pics/objects/manacrystal_blank.png", gp.tileSize, gp.tileSize);
+        image = setup("/pics/objects/manacrystal_full.png", gp.tileSize/2, gp.tileSize/2);
+        image2 = setup("/pics/objects/manacrystal_blank.png", gp.tileSize/2, gp.tileSize/2);
     }
 }
