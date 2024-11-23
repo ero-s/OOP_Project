2077c45a2e6dc93abcaa2b25f69f7bd041a44759
ero-s <austinelomcso@gmail.com>
2024-11-23 19:50:24 +0800

    new implementations

---
+++ src/object/BUFF_Cabbage.java
@@ -0,0 +1,4 @@
+package object;
+
+public class BUFF_Cabbage {
+}
---
+++ src/object/CON_Carrot.java
@@ -0,0 +1,23 @@
+package object;
+
+import Entity.Entity;
+import pkg2dgame.GamePanel;
+
+public class BUFF_Carrot extends Entity{
+    public BUFF_Carrot(GamePanel gp){
+        super(gp);
+
+        name = "Carrot";
+        down1 = setup("/pics/objects/carrot_buff.png",gp.tileSize/2, gp.tileSize/2);
+        description = "[" + name + "]\n Increases Health.";
+        defenseValue = 3;
+        collision = true;
+        solidArea.x = 64;
+        solidArea.y = 64;
+        solidArea.width = 32;
+        solidArea.height = 16;
+        xOffset = 0;
+        yOffset = 0;
+        setCollisionArea(solidArea.x, solidArea.y, solidArea.width, solidArea.height, 0, 16);
+    }
+}
